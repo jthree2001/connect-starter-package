c7dc1ee7027154cf89d7c249fcb23915f8018c59
David Le <davidhle@gatech.edu>
2018-06-12 09:23:19 -0400

    Lint and update readme

--- lib/workers/subscription_query_worker.rb
+++ lib/workers/subscription_query_worker.rb
@@ -4,16 +4,11 @@ class SubscriptionQueryWorker
 	def self.queue
 		@queue = :subscription_query_worker
 	end
-	
+
 	def self.perform(appinstance_id)
 		appinstance = ZuoraConnect::AppInstance.find(appinstance_id)
-    	appinstance.new_session()
- 		
- 		subscription_query = SubscriptionQuery.new(appinstance)
- 		subscription_query.execute
- 
+		appinstance.new_session()
+		subscription_query = SubscriptionQuery.new(appinstance)
+		subscription_query.execute
 	end
-
-
-
 end
